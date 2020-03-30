27a9af884ea5429d807b7e2554e74253d7d13492
Iuri Matarazzo Wollmann <iurimw@hotmail.com>
2020-03-30 02:12:33 -0300

    receiving insomnia json from args

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # integration-tests-generator
 Generate integration tests from insomnia json data
+
+For testing this script run: ``npm start -- --sample ./Insomnia_sample.json``
---
+++ src/generator.js
@@ -0,0 +1,35 @@
+const { exec } = require('child_process');
+
+module.exports = async function generate(resources) {
+    let results = [];
+
+    const requests = resources.filter(x=> x._type === 'request'); //get all requests
+    const groups = resources.filter(x=> x._type === 'request_group'); //get all groups
+    
+    const unparentRequests = requests.filter(x => x.parentId.substr(0,3) === "wrk");
+    groups.forEach(requestGroup => {
+        const r = requests.filter(x=> x.parentId === requestGroup._id);
+        results.push({ group : requestGroup, requests: r });
+    });
+    
+    unparentRequests.forEach(r => {
+        results.push({ group : r, requests: [ r ]});
+    });
+    
+    try {
+        results.forEach(result => {
+            exec(`hygen generator tests '${encodeURIComponent(JSON.stringify(result))}'`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    return;
+                }
+            });
+        });    
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+}
